a81e99df01887cfbc5fe204f256eb4e3ea89b78a
aaaaryaannn <91652136+aaaaryaannn@users.noreply.github.com>
2023-02-16 00:45:41 +0530

    frontend

---
+++ fleetgen/script.js
@@ -0,0 +1,9 @@
+let slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+let slideInterval = setInterval(nextSlide, 5000);
+
+function nextSlide() {
+  slides[currentSlide].classList.remove('active');
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.add('active');
+}
--- fleetgenius/src/config.js
+++ fleetgenius/src/config.js
@@ -3,11 +3,12 @@
 const config = {
   apiKey: "AIzaSyCANuczAsCo7MObCwctuEgGkyJvjkj0IEw",
   authDomain: "fleetgenius-3eba1.firebaseapp.com",
+  databaseURL: "https://fleetgenius-3eba1-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "fleetgenius-3eba1",
   storageBucket: "fleetgenius-3eba1.appspot.com",
   messagingSenderId: "7334360112",
-  appId: "1:7334360112:web:45a967a0afd8a1050de9f5",
-  measurementId: "G-MCX2HHYLBS"
+  appId: "1:7334360112:web:d178ffef88e48eae0de9f5",
+  measurementId: "G-ZVZE56T77W"
 };
 
 

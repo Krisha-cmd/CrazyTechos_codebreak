ec310dd2b206a7f6a9484392598530f50bf3f602
aaaaryaannn <91652136+aaaaryaannn@users.noreply.github.com>
2023-02-15 23:11:39 +0530

    Update Connector.js

--- fleetgenius/src/Connector.js
+++ fleetgenius/src/Connector.js
@@ -1,9 +1,11 @@
-import React from 'react';
+/*import React from 'react';
 import './App.css';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth'; 
 import 'firebase/database';
 import config from './config';
+import { Login } from './login';
+
 
 class Connector extends React.Component {
   constructor(props) {
@@ -74,25 +76,11 @@ class Connector extends React.Component {
       <div>
         {isLoggedIn ? (
           <div>
-            <h2>You are logged in!</h2>
             <button onClick={this.handleLogout}>Logout</button>
           </div>
         ) : (
           <div>
-            <input
-              type="email"
-              name="myemail"
-              placeholder="Email"
-              onChange={this.handleInputChange}
-            />
-            <input
-              type="password"
-              name="mypswd"
-              placeholder="Password"
-              onChange={this.handleInputChange}
-            />
-            <button onClick={this.handleLogin}>Login</button>
-            {error && <p>{error}</p>}
+            <Login/>
           </div>
         )}
       </div>
@@ -100,4 +88,92 @@ class Connector extends React.Component {
   }
 }
 
-export default Connector
+export default Connector;*/
+
+import React from 'react';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
+const config = {
+  // Your Firebase config goes here
+};
+
+class SignIn extends React.Component {
+  state = {
+    email: '',
+    password: '',
+    user: null,
+    error: null
+  }
+
+  componentDidMount() {
+    firebase.initializeApp(config);
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ user: user });
+      } else {
+        this.setState({ user: null });
+      }
+    });
+  }
+
+  handleEmailChange = (e) => {
+    this.setState({ email: e.target.value });
+  }
+
+  handlePasswordChange = (e) => {
+    this.setState({ password: e.target.value });
+  }
+
+  handleSignIn = () => {
+    firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+      .then((userCredential) => {
+        // User signed in successfully
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        // Handle errors
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        this.setState({ error: errorMessage });
+        console.error(errorCode, errorMessage);
+      });
+  }
+
+  handleSignOut = () => {
+    firebase.auth().signOut()
+      .then(() => {
+        // User signed out successfully
+        console.log('User signed out');
+      })
+      .catch((error) => {
+        // Handle errors
+        console.error(error);
+      });
+  }
+
+  render() {
+    if (this.state.user) {
+      // User is signed in
+      return (
+        <div>
+          <p>Welcome, {this.state.user.displayName}!</p>
+          <button onClick={this.handleSignOut}>Sign Out</button>
+        </div>
+      );
+    } else {
+      // User is signed out
+      return (
+        <div>
+          <input type="email" value={this.state.email} onChange={this.handleEmailChange} />
+          <input type="password" value={this.state.password} onChange={this.handlePasswordChange} />
+          <button onClick={this.handleSignIn}>Sign In</button>
+          {this.state.error && <p>{this.state.error}</p>}
+        </div>
+      );
+    }
+  }
+}
+
+export default SignIn;

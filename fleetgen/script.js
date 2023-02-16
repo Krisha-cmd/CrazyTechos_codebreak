f228f21b707f2a258fe444c4855d09637b3beba4
aaaaryaannn <91652136+aaaaryaannn@users.noreply.github.com>
2023-02-16 05:39:29 +0530

    add group feature

--- fleetgen/script.js
+++ fleetgen/script.js
@@ -7,3 +7,19 @@
       map_canvas.classList.add("shorter");
 			chatslider.classList.add("longer");
 		}*/
+    var popup=document.getElementById("newgroup");
+    function openPopup() {
+      
+    var popupscreen=document.getElementById("popup-container");
+      popupscreen.style.display="block";
+    }
+
+    function exitPop(){
+      var popupscreen=document.getElementById("popup-container");
+      popupscreen.style.display="none";
+    }
+
+    function selected(id){
+      var id=document.getElementById(id);
+      id.style.color="#98DF94";
+    }
